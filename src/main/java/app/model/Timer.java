08dc6906dbd11fab56416ce18fb32c12e6433cd9
anmizantar <anmizantar@gmail.com>
2025-03-10 09:50:00 +0300

    Initial commit: добавлен проект

--- src/main/java/app/model/Timer.java
+++ src/main/java/app/model/Timer.java
@@ -1,10 +1,15 @@
 package app.model;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+
 public class Timer {
 
-    private Long nanoTime = System.nanoTime();
+    private static final Long fixedTime = System.nanoTime();
 
     public Long getTime() {
-        return nanoTime;
+        return fixedTime;
     }
 }
